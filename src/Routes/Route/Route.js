2a6800d5b67b30e7a66c9445205cc2f5f545fd59
Raihan <rhnchdri@gmail.com>
2022-11-24 21:35:15 +0600

    navbar and header part

---
+++ src/Routes/Route/Route.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../Layouts/Main';
+import Home from '../../Pages/Homes/Home';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+]);
